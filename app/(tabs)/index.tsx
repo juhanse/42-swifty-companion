fc4e2240e33c3d7ab8481e5f03381556b504c9cc
juhanse <julien.hanse12@gmail.com>
2025-12-15 16:50:35 +0100

    fix: index search display

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,36 @@
-import { useState, useEffect } from 'react';
-import { View, StyleSheet, Image, Text } from 'react-native';
-import { User, fetchUser } from '@/services/users'
+import React, { useState } from 'react'
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { router } from 'expo-router';
+import { useTranslation } from 'react-i18next';
 
 export default function HomeScreen() {
-	const [user, setUser] = useState<User | null>(null);
+	const { t } = useTranslation();
+	const [login, setLogin] = useState<string | null>(null);
 
-	useEffect(() => {
-		fetchUser({ login: 'juhanse' })
-			.then(setUser)
-			.catch(console.error);
-	}, [fetchUser]);
+	const handlePress = (groupId: string) => {
+		if (groupId.length < 3) {
+			return ;
+		}
+
+		router.push({ pathname: "/(tabs)/profile/[id]", params: { id: groupId } });
+	};
 
 	return (
 		<View style={styles.container}>
-			<View style={styles.header}>
-				<Text>
-					{user?.displayname}
-				</Text>
-				<Image src={user?.url} style={styles.avatar}/>
+			<TextInput
+				style={styles.input}
+				placeholder={t('search')}
+				placeholderTextColor="rgba(162,162,162,0.5)"
+				value={login || ''}
+				onChangeText={setLogin}
+				multiline={true}
+				autoCorrect={false}
+			/>
+
+			<View style={[styles.buttonContainer]}>
+				<Button type="primary" onPress={handlePress} disabled={!login || login.length < 3}>
+					{t('search')}
+				</Button>
 			</View>
 		</View>
     );
@@ -27,12 +40,20 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 	},
-	header: {
-		width: '100%',
-		marginVertical: 55,
+	input: {
+		flex: 1,
+		height: 50,
+		paddingHorizontal: 0,
+		fontSize: 32,
+		fontFamily: 'SF-Semibold',
+		color: 'rgba(162,162,162,0.8)',
+		backgroundColor: 'transparent',
 	},
-	avatar: {
+	buttonContainer: {
 		width: '100%',
-		height: '100%',
-	}
+		position: 'absolute',
+		top: 750,
+		flexDirection: 'column',
+		paddingHorizontal: 80,
+	},
 });
