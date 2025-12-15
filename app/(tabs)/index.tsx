d1f22d4888913b77beaf5c76547dbf7613f13f99
juhanse <julien.hanse12@gmail.com>
2025-12-15 15:51:20 +0100

    add: display basic info

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image, Text } from 'react-native';
 import { User, fetchUser } from '@/services/users'
 
 export default function HomeScreen() {
@@ -9,17 +9,30 @@ export default function HomeScreen() {
 		fetchUser({ login: 'juhanse' })
 			.then(setUser)
 			.catch(console.error);
-	}, []);
+	}, [fetchUser]);
 
 	return (
 		<View style={styles.container}>
-
+			<View style={styles.header}>
+				<Text>
+					{user?.displayname}
+				</Text>
+				<Image src={user?.url} style={styles.avatar}/>
+			</View>
 		</View>
     );
 }
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1
+		flex: 1,
+	},
+	header: {
+		width: '100%',
+		marginVertical: 55,
+	},
+	avatar: {
+		width: '100%',
+		height: '100%',
 	}
 });
