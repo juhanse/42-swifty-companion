d844bcdc2be77e6333a8e50768362db5728c4435
juhanse <julien.hanse12@gmail.com>
2025-12-15 12:58:16 +0100

    add: api service

---
+++ services/api.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { tokenService } from '@/services/token';
+
+export const api42 = axios.create({
+	baseURL: 'https://api.intra.42.fr',
+});
+
+api42.interceptors.request.use(async config => {
+	const token = await tokenService.getAccessToken();
+	config.headers.Authorization = `Bearer ${token}`;
+	return config;
+});
+
+api42.interceptors.response.use(
+	res => res,
+	async error => {
+		if (error.response?.status === 401) {
+			const newToken = await tokenService.refreshToken();
+			error.config.headers.Authorization = `Bearer ${newToken}`;
+			return api42.request(error.config);
+		}
+		return Promise.reject(error);
+	}
+);
