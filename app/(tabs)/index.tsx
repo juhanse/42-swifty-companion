4fedbad8889018431f72515d2f6e7cffdc2f8d70
juhanse <julien.hanse12@gmail.com>
2025-12-15 13:55:59 +0100

    add: fetchUser

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,16 @@
+import { useState, useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
+import { User, fetchUser } from '@/services/users'
 
 export default function HomeScreen() {
+	const [user, setUser] = useState<User | null>(null);
+
+	useEffect(() => {
+		fetchUser({ login: 'juhanse' })
+			.then(setUser)
+			.catch(console.error);
+	}, []);
+
 	return (
 		<View style={styles.container}>
 
