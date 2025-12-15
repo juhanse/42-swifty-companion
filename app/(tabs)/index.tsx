47952122c996944f895e1e8c2a900c9cafaa7253
juhanse <julien.hanse12@gmail.com>
2025-12-15 21:12:02 +0100

    fix: display HomeScreen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
-import { View, TextInput, StyleSheet } from 'react-native';
+import { View, TextInput, Platform, KeyboardAvoidingView, StyleSheet } from 'react-native';
 import { router } from 'expo-router';
 import { useTranslation } from 'react-i18next';
+import { Background } from '@/components/Background';
 import Button from '@/components/Button'
 
 export default function HomeScreen() {
@@ -17,43 +18,54 @@ export default function HomeScreen() {
 	};
 
 	return (
-		<View style={styles.container}>
-			<TextInput
-				style={styles.input}
-				placeholder={t('search')}
-				placeholderTextColor="rgba(162,162,162,0.5)"
-				value={login || ''}
-				onChangeText={setLogin}
-				autoCorrect={false}
-			/>
+		<KeyboardAvoidingView
+			style={{ flex: 1 }}
+			behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+			keyboardVerticalOffset={Platform.OS === 'ios' ? 1 : 0}
+		>
+			<Background />
+
+			<View style={styles.container}>
+				<TextInput
+					style={styles.input}
+					placeholder={t('search')}
+					placeholderTextColor="rgba(162,162,162,0.5)"
+					value={login || ''}
+					onChangeText={setLogin}
+					autoCorrect={false}
+				/>
+			</View>
 
 			<View style={[styles.buttonContainer]}>
 				<Button type="primary" onPress={() => handlePress(login!)} disabled={!login || login.length < 3}>
 					{t('search')}
 				</Button>
 			</View>
-		</View>
+		</KeyboardAvoidingView>
     );
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		justifyContent: 'flex-start',
+		alignItems: 'center',
+		gap: 40,
+		paddingTop: 80,
+		paddingHorizontal: 40,
 	},
 	input: {
 		flex: 1,
 		height: 50,
-		paddingHorizontal: 0,
 		fontSize: 32,
 		fontFamily: 'SF-Semibold',
-		color: 'rgba(162,162,162,0.8)',
+		color: 'rgba(222, 222, 222, 0.8)',
 		backgroundColor: 'transparent',
 	},
 	buttonContainer: {
 		width: '100%',
-		position: 'absolute',
-		top: 750,
 		flexDirection: 'column',
 		paddingHorizontal: 80,
+		paddingBottom: 40,
 	},
 });
