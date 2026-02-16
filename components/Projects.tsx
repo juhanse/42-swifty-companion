83eec0ec74ceeccc2cfbc60d0847e6c9a9e74506
juhanse <julien.hanse12@gmail.com>
2026-02-16 21:16:26 +0100

    add: Projects component

---
+++ components/Projects.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { FlatList, View, StyleSheet, Text } from 'react-native';
+import { ProjectCard } from '@/components/ui/ProjectCard';
+import { ProjectUser } from '@/services/projects';
+
+interface ProjectsProps {
+    projects: ProjectUser[];
+}
+
+export const Projects = ({ projects }: ProjectsProps) => {
+    const finishedProjects = projects
+        .filter(p => p.status === 'finished')
+        .sort((a, b) => new Date(b.marked_at).getTime() - new Date(a.marked_at).getTime());
+
+    return (
+        <View style={styles.wrapper}>
+			<Text style={styles.title}>Projets :</Text>
+            <FlatList
+                data={finishedProjects}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                keyExtractor={(item) => item.id.toString()}
+                contentContainerStyle={styles.listContent}
+                snapToInterval={270}
+                decelerationRate="fast"
+                renderItem={({ item }) => (
+                    <ProjectCard projectUser={item} />
+                )}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    wrapper: {
+		paddingVertical: 20,
+    },
+	title: {
+		fontSize: 20,
+		fontFamily: "SF-Bold",
+		color: "#fff",
+		marginBottom: 10,
+	},
+    listContent: {
+        gap: 10,
+    },
+});
