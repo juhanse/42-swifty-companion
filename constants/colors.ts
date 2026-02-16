802e9eb23ce61585218f0bf94c87d53cbeb918d6
juhanse <julien.hanse12@gmail.com>
2026-02-16 19:43:24 +0100

    add: colors badge

--- constants/colors.ts
+++ constants/colors.ts
@@ -4,9 +4,12 @@ export const Colors = {
 	badge: {
 		["Tutor"]: '#0078FF',
 		["Supervising Tutor"]: '#6000BC',
-		["Staff"]: '#E05757',
+		["STAFF"]: '#E05757',
 		["BDE"]: '#2E657C',
 		["Phoenix"]: '#F99E3B',
 		["Pegasus"]: '#0F172A',
+		["Prefect"]: '#DD2B84',
+		["Test account"]: '#D63031',
+		["alumni"]: '#00BAC4',
 	}
 };
