a3d505692f9dd9c55db4df781bf32bf80ce7c571
juhanse <julien.hanse12@gmail.com>
2026-06-29 22:43:37 +0200

    delete: initial layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,5 @@
 import { Stack } from 'expo-router';
 import { useFonts } from 'expo-font';
-import { I18nextProvider } from 'react-i18next';
-import i18n from '@/hooks/useTranslation';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
-
-const InitialLayout = () => {
-	return (
-		<Stack screenOptions={{ headerShown: false }}>
-			<Stack.Screen name="(tabs)" />
-		</Stack>
-	);
-}
 
 export default function RootLayout() {
 	const [loaded] = useFonts({
@@ -26,10 +13,8 @@ export default function RootLayout() {
 	}
 
 	return (
-		<QueryClientProvider client={queryClient}>			
-			<I18nextProvider i18n={i18n}>
-				<InitialLayout />
-			</I18nextProvider>
-		</QueryClientProvider>
-	)
+		<Stack screenOptions={{ headerShown: false }}>
+			<Stack.Screen name="(tabs)" />
+		</Stack>
+	);
 }
